583501ec0a994b3718d312b0347c04082ccecf20
Dalibor Vaněk <dalibor.vanek@gmail.com>
2021-04-02 12:41:32 +0200

    Add ParcelShops

---
+++ src/PPL/PPLClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hefferon.CzechServices.PPL
+{
+    public class PPLClient
+    {
+        public PPLClient()
+        {
+
+        }
+
+        public List<PPLServiceReference.KTMDetail> GetAllPickUpPlaces()
+        {
+            PPLServiceReference.KtmSoapClient client = new PPLServiceReference.KtmSoapClient(new PPLServiceReference.KtmSoapClient.EndpointConfiguration());
+            var result = client.GetKTMListAsync("CZ").Result.ToList();
+            return result;
+        }
+    }
+}
---
+++ src/Test/PPLClientTest.cs
@@ -0,0 +1,19 @@
+﻿using Hefferon.CzechServices.CNB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Hefferon.CzechServices.Test
+{
+    [TestClass]
+    public class PPLClientTest
+    {
+
+        [TestMethod]
+        public void GetPickupPlacesTest()
+        {
+            var client = new Hefferon.CzechServices.PPL.PPLClient();
+            var result = client.GetAllPickUpPlaces();
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
